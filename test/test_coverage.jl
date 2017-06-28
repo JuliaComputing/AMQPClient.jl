daf49a8403fd34cb459c8fd8bf20e37df0e251dc
tan <tanmaykm@gmail.com>
2017-06-28 12:13:36 -0700

    tests for fix #8

--- test/test_coverage.jl
+++ test/test_coverage.jl
@@ -65,6 +65,8 @@ function runtests(;virtualhost="/", host="localhost", port=AMQPClient.AMQP_DEFAU
         @test rcvd_msg.redelivered == false
         @test rcvd_msg.routing_key == ROUTE1
         @test rcvd_msg.data == M.data
+        @test :content_type in keys(rcvd_msg.properties)
+        @test convert(String, rcvd_msg.properties[:content_type]) == "text/plain"
     end
 
     # basic get returns null if no more messages
